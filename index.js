91f42d26f73d58a54a018678c26c29e61d9b1147
Aaron Cunnington <azcn2503@gmail.com>
2017-03-13 11:34:44 +0000

    Remove Lodash dependency.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-var _ = require("lodash");
-
 module.exports = function(str) {
-  var chars = "[a-zA-Z0-9\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\_\\`\\{\\|\\}\\~\\@\\.]+";
+  var chars = "[a-zA-Z0-9\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\_\\`\\{\\|\\}\\~\\\.]+";
   var emailRegExp = new RegExp(chars + "?\@" + chars + "?\\." + chars, "g");
   var emails = str.match(emailRegExp);
-  return _.chain(emails)
-    .map(function(email) { return email.toLowerCase(); })
-    .filter(function(email) { return /.\@./.test(email) && !/\.\./.test(email); })
-    .uniq()
-    .value();
+  var uniqueEmails = {};
+  for (var i = 0; i < emails.length; i += 1) {
+    if (/\.\./.test(emails[i])) {
+      emails.pop(i);
+      continue;
+    }
+    uniqueEmails[emails[i].toLowerCase()] = true;
+  }
+  emails = [];
+  for (var i in uniqueEmails) {
+    if (!uniqueEmails.hasOwnProperty(i)) {
+      continue;
+    }
+    emails.push(i);
+  }
+  return emails;
 };
